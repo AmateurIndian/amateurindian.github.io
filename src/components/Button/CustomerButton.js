f770d4a33a96a3c0f09d3181250a3a85131d5201
amateurindian <sarj93@hotmail.com>
2021-05-16 15:31:44 +0300

    Revert "Updated CV Link"
    This reverts commit 7c905ccdbdb5319e785626b31e7ff0c8855634ea.

--- src/components/Button/CustomerButton.js
+++ src/components/Button/CustomerButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import  {Button} from '@material-ui/core' 
 import './CustomButton.css'
+import pdf from '../../assets/documents/syusuf_CV.pdf'
 
 const CustomButton = ({text, icon, link}) => {
     return (
        <Button 
         className= {'custom_btn'} 
-        href =  'https://sarjeelyusufcv.s3.eu-central-1.amazonaws.com/syusuf_CV.pdf'
+        href =  {pdf}
         target = "_blank"
         endIcon={icon ? 
         (<div className='btn_icon_container'>
